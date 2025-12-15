e76aa94a0294f52423860c029d7d073b96bfa73e
CODACCIONI Kilian <HO.KCODACCIONI@cma-cgm.com>
2025-12-15 19:53:07 +0100

    multiplayer logic

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
     return (
         <div id="canvas-container">
             <ErrorBoundary>
-                <Canvas orthographic camera={{ zoom: 180, position: [0, 0, 100], near: 0.1, far: 1000 }}>
+                <Canvas camera={{ position: [0, 8, 6], fov: 60 }}>
                     <color attach="background" args={['#1a1a1a']} />
                     <ambientLight intensity={0.5} />
                     <directionalLight position={[10, 10, 5]} intensity={1} />
